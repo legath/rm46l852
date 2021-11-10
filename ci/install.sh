5db4f6e3295c0e4e62999e63999c53e207e98238
Alexander Bulychev <a.f.bulychev@gmail.com>
2021-11-10 03:20:42 +0300

    Changes for little endian ci

--- ci/install.sh
+++ ci/install.sh
@@ -2,7 +2,7 @@ set -euxo pipefail
 
 main() {
     case $TARGET in
-        armebv7r-none-eabi*)
+        armv7r-none-eabi*)
             rustup target add $TARGET
             ;;
     esac
--- ci/script.sh
+++ ci/script.sh
@@ -5,7 +5,7 @@ main() {
     arm-none-eabi-gcc --version
 
     case $TARGET in
-        armebv7r-none-eabi*)
+        armv7r-none-eabi*)
             if [ $TRAVIS_RUST_VERSION = nightly ]; then
                 cargo check --target $TARGET
                 cargo check --target $TARGET --release
